194f716789ca52e3eaaecda3970ae6a6c4a30720
Akshay Suresh <as3655@cornell.edu>
2019-12-30 03:20:22 -0500

    Added I/O for config file inputs

--- psrdynspec/io/__init__.py
+++ psrdynspec/io/__init__.py
@@ -1,4 +1,5 @@
 from .parse_sp import read_singlepulse, gen_singlepulse, remove_duplicates, apply_sigma_cutoff
+from .read_config import read_config
+from .read_fil import load_fil_data
 from .read_header import Header
 from .read_psrfits import load_psrfits_data
-from .read_fil import load_fil_data
