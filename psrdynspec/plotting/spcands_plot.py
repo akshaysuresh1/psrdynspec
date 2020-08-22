7c45c37945421de2da4ed10c754633dfb0ace51c
Akshay Suresh <as3655@cornell.edu>
2020-08-22 02:41:50 -0400

    Added function to plot single pulse cands in DM-time plane

--- psrdynspec/plotting/spcands_plot.py
+++ psrdynspec/plotting/spcands_plot.py
@@ -56,3 +56,93 @@ def plot_diag_cands(cand_dedisp_times,cand_DMs,unique_cand_dedisp_times,unique_c
     else:
         plt.close()
 #########################################################################
+# Reproduce plot of single pulse detections in the DM-time plane.
+'''
+Inputs:
+cand_dedisp_times = 1D array of times (s) of single pulse candidates
+cand_DMs = 1D array of DMs (pc/cc) of single pulse candidates
+cand_sigma = 1D array of detection S/N of single pulse candidates
+metadata = infodata object containing observation metadata
+SAVE_DIR = Path to which output plot must be saved (d: working directory)
+show_plot = Do you want to show the plot live? (True/False) (default = False)
+low_DM_cand = Min. DM (pc/cc) to plot (d: np.min(cand_DMs))
+high_DM_cand = Max DM (pc/cc) to plot (d: np.max(cand_DMs))
+'''
+def plot_DMtime(cand_dedisp_times, cand_DMs, cand_sigma, metadata, SAVE_DIR='', show_plot=False, low_DM_cand=None, high_DM_cand=None):
+    if low_DM_cand is None:
+        low_DM_cand = np.min(cand_DMs)
+    if high_DM_cand is None:
+        high_DM_cand = np.min(cand_DMs)
+
+    plot_name = metadata.basename
+    if '/' in plot_name:
+        plot_name = plot_name.split('/')[-1]
+    if 'DM' in plot_name:
+        plot_name = plot_name.split('_DM')[0]
+
+    print('Reconstructing single_pulse_search.py output')
+    fig = plt.figure(figsize=(10,8))
+
+    # Make outer gridspec,
+    outer = gridspec.GridSpec(2, 1, figure=fig,height_ratios = [1, 2])
+    # Plot DM-time plane in bottom gridspec.
+    gs1 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec = outer[1])
+    ax1 = plt.subplot(gs1[0])
+    ax1.scatter(x=cand_dedisp_times,y=cand_DMs,s=(cand_sigma**2.)/5,marker='o',facecolor='None',edgecolor='k')
+    ax1.set_ylim((low_DM_cand, high_DM_cand))
+    ax1.set_xlabel('Time (s)',fontsize=14)
+    ax1.set_ylabel('DM (pc cm$^{-3}$)',fontsize=14)
+    # Split top gridspec into three subplots.
+    gs0 = gridspec.GridSpecFromSubplotSpec(1, 3, subplot_spec = outer[0], width_ratios=[1,1,1],wspace=0.3)
+    # Left subplot: Histogram of candidate S/N
+    ax00 = plt.subplot(gs0[0])
+    ax00.hist(cand_sigma, bins='auto', facecolor='None', edgecolor='k', histtype='step')
+    ax00.set_xlim((np.min(cand_sigma),np.max(cand_sigma)))
+    ax00.set_ylabel('No. of pulses per bin',fontsize=14)
+    ax00.set_xlabel('S/N',fontsize=14)
+    # Middle subplot: Histogram of candidate DMs
+    ax01 = plt.subplot(gs0[1])
+    ax01.hist(cand_DMs, bins='auto', facecolor='None', edgecolor='k', histtype='step')
+    ax01.set_xlim((low_DM_cand, high_DM_cand))
+    ax01.set_ylabel('No. of pulses per bin',fontsize=14)
+    ax01.set_xlabel('DM (pc cm$^{-3}$)',fontsize=14)
+    # Right subplot: Scatter plot candidate S/N vs. candidate DMs
+    ax02 = plt.subplot(gs0[2])
+    ax02.scatter(x=cand_DMs, y=cand_sigma, s=2, marker='o', facecolor='None', edgecolor='k')
+    ax02.set_xlim((low_DM_cand, high_DM_cand))
+    ax02.set_ylim((np.min(cand_sigma),np.max(cand_sigma)))
+    ax02.set_xlabel('DM (pc cm$^{-3}$)',fontsize=14)
+    ax02.set_ylabel('S/N', fontsize=14)
+    fig.subplots_adjust(left=0.08, right=0.92, top=0.8, bottom=0.08)
+
+    # Metadata
+    # Column 0
+    ax00.annotate('Source: %s'% (metadata.object),xy=(0.0, 1.5), xycoords='axes fraction',fontsize=14)
+    ax00.annotate('Telescope: %s'% (metadata.telescope),xy=(0.0, 1.3), xycoords='axes fraction',fontsize=14)
+    ax00.annotate('Instrument: %s'% (metadata.instrument),xy=(0.0, 1.1), xycoords='axes fraction',fontsize=14)
+    # Column 1
+    ax01.annotate('RA (J2000) = %s'% (metadata.RA),xy=(0.0, 1.5),xycoords='axes fraction',fontsize=14)
+    if '-' in metadata.DEC:
+        DEC_label = r'$-$' + metadata.DEC.split('-')[1]
+    elif '+' in metadata.DEC:
+        DEC_label = r'$+$' + metadata.DEC.split('+')[1]
+    else:
+        DEC_label = r'$+$' + metadata.DEC
+    ax01.annotate('Dec (J2000) = %s'% (DEC_label),xy=(0.0, 1.3),xycoords='axes fraction',fontsize=14)
+    if metadata.bary:
+        MJD_label = r'MJD$_{\mathrm{bary}}$'
+    else:
+        MJD_label = r'MJD$_{\mathrm{topo}}$'
+    ax01.annotate(MJD_label+' = %.12g'% (metadata.epoch),xy=(0.0, 1.1),xycoords='axes fraction',fontsize=14)
+    # Column 2
+    ax02.annotate('No. of samples = %d'% (metadata.N),xy=(0.0, 1.5), xycoords='axes fraction',fontsize=14)
+    ax02.annotate('Sampling time = %.2f $\mu$s'% (metadata.dt*1e6),xy=(0.0, 1.3), xycoords='axes fraction',fontsize=14)
+    ax02.annotate('Freq$_{\mathrm{ctr}}$ = %.1f MHz'% (center_freq),xy=(0.0, 1.1),xycoords='axes fraction',fontsize=14)
+
+    # Save plot and display it live, if specified.
+    plt.savefig(SAVE_DIR+plot_name+'_DMtime.png')
+    if show_plot:
+        plt.show()
+    else:
+        plt.close()    
+#########################################################################
