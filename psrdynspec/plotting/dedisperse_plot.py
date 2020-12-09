730945828b6e889cbd16bfec47d9a96eae5e4edf
Akshay Suresh <akshay721@gmail.com>
2020-12-08 23:52:38 -0500

    Updated plotting routine.

--- psrdynspec/plotting/dedisperse_plot.py
+++ psrdynspec/plotting/dedisperse_plot.py
@@ -92,33 +92,32 @@ def plot_dedisp_subband_SNRvsDM(dedisp_ds,dedisp_timeseries,dedisp_times,freqs_a
 # Combine plots of the entire dedispersed dynamic spectrum, dedispersed time series and S/N vs. DM plot.
 '''
 Inputs:
-whole_ds = 2D array, DM = 0 pc/cc dynamic spectrum
+whole_ds = 2D array, non-dedispersed dynamic spectrum
 times = 1D array of times (s) covered in non-dedispersed dynamic spectrum
-dedisp_ds = 2D array, dedispersed dynamic spectrum
-dedisp_timeseries = Sum of dedisp_ds over frequency
+freqs_array = 1D array of frequencies (GHz) covered in dynamic spectrum
+dedisp_ds = 2D array, dynamic spectrum dedispersed at optimal DM
+dedisp_timeseries = 1D array, sum of dedisp_ds over frequency
 dedisp_times = 1D array of times(s) covered in dedisp_timeseries
-freqs_array = 1D array of frequencies (GHz) covered in dedisp_ds
-t_cand = Time (s) of occurrence of candidate
-t_resol  = Time resolution (s) of data
 trial_DMs = 1D array of trial DMs at which dedispersion must be performed
 SNR = 1D array of signal-to-noise ratios at above trial DMs
-optimal_DM = DM that maximized S/N
-offpulse_std_value = Off-pulse standard deviation to normalize the dedispersed time series
-flux_unit = Unit of flux density
-freq_unit = Unit of radio frequency (usually GHz) for plotting
-time_offset_unit = Unit of time (usually s) for plotting
-timeoffset_conversion_factor = Conversion factor (float) from time units to time offset units. For example, s -> ms x 1.e3
+optimal_DM = DM (pc/cc) that maximizes the S/N
+t_cand = Time (s) of occurrence of candidate
+offpulse_std_value = Off-pulse standard deviation to normalize the dedispersed time series (d: 1.)
+SN_smooth = 1D array, Smoothed S/N vs. trial DM curve (d: None)
 basename = Basename (string) for output plot (including path)
-show_plot = Do you want to show the plot live? (True/False) (default = False)
-vmin = Min. color bar axis value for flux density (default = np.nanmin(whole_ds))
-vmax = Max color bar axis value for flux density (default = np.nanmax(whole_ds))
-log_colorbar = Do you want a log-spaced colorbar? (True/False) (default = False)
+show_plot = Do you want to view the plot live? (True/False) (d: False)
+vmin = Min. color bar axis value for flux density (d: np.nanmin(whole_ds))
+vmax = Max color bar axis value for flux density (d: np.nanmax(whole_ds))
+log_colorbar = Do you want a log-spaced colorbar? (True/False) (d: False)
+flux_unit = Unit of flux density (d: arb. units)
+freq_unit = Unit of radio frequency (d: GHz)
+time_unit = Unit of time (d: s)
 '''
-def plot_dedisp_ds_SNRvsDM(whole_ds,times,dedisp_ds,dedisp_timeseries,dedisp_times,freqs_array,t_cand,t_resol,trial_DMs,SNR,optimal_DM,offpulse_std_value,flux_unit,freq_unit='GHz',time_offset_unit='s',timeoffset_conversion_factor=1.0,basename='',show_plot=False,vmin=None,vmax=None,log_colorbar=False):
+def plot_dedisp_ds_SNRvsDM(whole_ds,times,freqs_array,dedisp_ds,dedisp_timeseries,dedisp_times,trial_DMs,SNR,optimal_DM,t_cand,offpulse_std_value=1.0,SN_smooth=None,basename='',show_plot=False,vmin=None,vmax=None,log_colorbar=False,flux_unit='arb. units',freq_unit='GHz',time_unit='s'):
     # Specify plot name.
     low_freq_limit = np.min(freqs_array)
     high_freq_limit = np.max(freqs_array)
-    plot_name = basename+'_t'+'%.3f'% (t_cand)+'_dedispDS_SNRvsDM_freqs'+'%.2f'% (low_freq_limit)+'to'+'%.2f'% (high_freq_limit)+'.png'
+    plot_name = basename+'_t'+'%.2f'% (t_cand)+'_dedispDS_SNRvsDM_freqs'+'%.2f'% (low_freq_limit)+'to'+'%.2f'% (high_freq_limit)+'.png'
 
     if (vmin is None):
         vmin = np.nanmin(whole_ds)
@@ -149,10 +148,12 @@ def plot_dedisp_ds_SNRvsDM(whole_ds,times,dedisp_ds,dedisp_timeseries,dedisp_tim
     print('Plotting S/N vs. DM')
     gs1 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec = outer[0])
     ax1 = plt.subplot(gs1[0])
-    ax1.plot(trial_DMs,SNR)
+    ax1.plot(trial_DMs,SNR, linestyle=':', color='k')
+    if SN_smooth is not None:
+        ax1.plot(trial_DMs, SN_smooth, linestyle=':', color='r', alpha=0.5)
     ax1.set_xlabel('Trial DM (pc cm$^{-3}$)',fontsize=16)
     ax1.set_ylabel('S/N',fontsize=16)
-    ax1.axvline(x=optimal_DM,label='DM$_{\mathrm{opt}}$',linestyle='--',color='r')
+    ax1.axvline(x=optimal_DM,label='DM$_{\mathrm{opt}}$',linestyle='--',color='sandybrown',alpha=0.5)
     ax1.legend(loc='best',prop={'size':14})
 
     # Construct nested gridspec within the bottom panel of outer gridspec.
@@ -160,43 +161,33 @@ def plot_dedisp_ds_SNRvsDM(whole_ds,times,dedisp_ds,dedisp_timeseries,dedisp_tim
     ax2 = plt.subplot(gs2[0])
     ax3 = plt.subplot(gs2[1],sharex=ax2)
 
-    # Determine time of pulse maximum.
-    pulse_max_time = dedisp_times[np.argmax(dedisp_timeseries)]
-    dedisp_time_offset = (dedisp_times -pulse_max_time)*timeoffset_conversion_factor
-    times_offset = (times - pulse_max_time)*timeoffset_conversion_factor
-
-    # Plot dedispersed time series in upper panel of bottom gridspec.
-    print('Padding dedispersed data products with NaNs to the time span of the non-dedispersed dynamic spectrum')
-    N_pad = whole_ds.shape[1] - np.size(dedisp_timeseries)
-    dedisp_timeseries = np.append(dedisp_timeseries, np.ones(N_pad)*np.NaN)
-    dedisp_ds = np.append(dedisp_ds, np.ones((len(whole_ds), N_pad))*np.NaN, axis=1)
-
     # Plot dedispersed time series in upper panel of bottom gridspec.
     print('Plotting dedispersed time series')
     dedisp_timeseries = dedisp_timeseries/offpulse_std_value # Convert dedispersed timeseries to S/N.
-    ax2.plot(times_offset,dedisp_timeseries)
-    ax2.set_ylabel('S/N',fontsize=16)
-
+    ax2.plot(dedisp_times,dedisp_timeseries)
+    ax2.set_ylabel('(S/N)$_{\mathrm{ts}}$',fontsize=16)
     # Plot the dedispersed dynamic spectrum in middle panel of bottom gridspec.
     print('Plotting dedispersed dynamic spectrum')
-    ds_ext = [times_offset[0],times_offset[-1],freqs_array[0],freqs_array[-1]]
+    ds_ext = [dedisp_times[0],dedisp_times[-1],freqs_array[0],freqs_array[-1]]
     if (log_colorbar==False):
         ax3.imshow(dedisp_ds,origin='lower',interpolation='nearest',aspect='auto',extent=ds_ext,vmin=vmin,vmax=vmax)
     else:
         ax3.imshow(dedisp_ds,origin='lower',interpolation='nearest',aspect='auto',extent=ds_ext,norm=LogNorm(vmin=vmin,vmax=vmax))
-    ax3.set_ylabel('Radio frequency ('+freq_unit+')',fontsize=16)
+    ax3.set_ylabel('Radio frequency (%s)'% (freq_unit),fontsize=16)
+    ax3.set_xlabel('Time (%s) referenced to %.2f %s'% (time_unit, freqs_array[-1], freq_unit))
+    ax3.set_xlim(dedisp_times[0],dedisp_times[-1])
 
+    # Plot non-dedispersed dynamic spectrum in lower panel of bottom gridspec.
     gs3 = gridspec.GridSpecFromSubplotSpec(1, 1, subplot_spec = outer[2])
     ax4 = plt.subplot(gs3[0],sharex=ax2)
-    # Plot non-dedispersed dynamic spectrum in lower panel of bottom gridspec.
     print('Plotting non-dedispersed dynamic spectrum')
     if (log_colorbar==False):
         im = ax4.imshow(whole_ds,origin='lower',interpolation='nearest',aspect='auto',extent=ds_ext,vmin=vmin,vmax=vmax)
     else:
         im = ax4.imshow(whole_ds,origin='lower',interpolation='nearest',aspect='auto',extent=ds_ext,norm=LogNorm(vmin=vmin,vmax=vmax))
-    ax4.set_ylabel('Radio frequency ('+freq_unit+')',fontsize=16)
-    ax4.set_xlabel('Time offset ('+time_offset_unit+') referenced to %.2f GHz'% (freqs_array[-1]),fontsize=16)
-    ax4.set_xlim(times_offset[0],times_offset[-1])
+    ax4.set_ylabel('Radio frequency (%s)'% (freq_unit),fontsize=16)
+    ax4.set_xlabel('Time (%s)' % (time_unit),fontsize=16)
+    ax4.set_xlim(times[0],times[-1])
 
     # Create space for colorbar on right side of plot.
     fig.subplots_adjust(right=0.80)
