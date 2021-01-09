135a4f9ac483ad4c6488f595e374611752319a60
Akshay Suresh <akshay721@gmail.com>
2021-01-09 05:45:45 -0500

    Default imports updated.

--- psrdynspec/__init__.py
+++ psrdynspec/__init__.py
@@ -17,12 +17,6 @@ except ImportError:
     print("Warning: Cannot import main processing utilities.")
 
 try:
-    from .plotting import config as plot_config
-    from .plotting import bandpass_plot, ds_plot, dedisperse_plot, fft_plot, fold_plot
-except ImportError:
-    print("Warning: Cannot import main plotting routines.")
-
-try:
     __version__ = get_distribution('psrdynspec').version
 except DistributionNotFound:
     __version__ = '0.0.0 - please install via setup.py'
